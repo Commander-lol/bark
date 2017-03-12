5a5d62ddcdf2f1a1719fbcc7d9574b8d9567a02b
Louis Capitanchik <contact@louiscap.co>
2017-03-12 20:20:48 +0000

    .env.example and some config shuffles

--- config/database.js
+++ config/database.js
@@ -1,23 +1,26 @@
 require('../core/bootstrap')
 
+// Sequelize cli expects env-namespaced config, but you should be using different env files
+// in different environments, so this will always pull from the environment.
+
 module.exports = {
 	'development': {
-		'username': 'root',
-		'password': 'password',
-		'database': 'bark_dev',
-		'host': '127.0.0.1',
-		'dialect': 'mysql',
+		'username': env('DB_USERNAME', 'root'),
+		'password': env('DB_PASSWORD'),
+		'database': env('DB_DATABASE', 'bark_dev'),
+		'host': env('DB_HOST', '127.0.0.1'),
+		'dialect': env('DB_ADAPTER', 'mysql'),
 	},
 	'test': {
-		'username': 'root',
-		'password': 'password',
-		'database': 'bark_test',
-		'host': '127.0.0.1',
-		'dialect': 'mysql',
+		'username': env('DB_USERNAME'),
+		'password': env('DB_PASSWORD'),
+		'database': env('DB_DATABASE', 'bark_test'),
+		'host': env('DB_HOST', '127.0.0.1'),
+		'dialect': env('DB_ADAPTER', 'mysql'),
 	},
 	'production': {
-		'username': env('DB_USERNAME', 'root'),
-		'password': env('DB_PASSWORD', 'password'),
+		'username': env('DB_USERNAME'),
+		'password': env('DB_PASSWORD'),
 		'database': env('DB_DATABASE', 'bark'),
 		'host': env('DB_HOST', '127.0.0.1'),
 		'dialect': env('DB_ADAPTER', 'mysql'),
--- core/app.js
+++ core/app.js
@@ -1,3 +1,5 @@
 const app = new (require('koa'))
 
+app.on('error', err => service.logger.error(err))
+
 module.exports = app
