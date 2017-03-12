ab367449794b2407b27ad954fa0bd9d6a4bfef57
Louis Capitanchik <contact@louiscap.co>
2017-03-12 18:58:39 +0000

    Add comment to services global

--- core/bootstrap.js
+++ core/bootstrap.js
@@ -2,12 +2,16 @@ require('dotenv').load()
 
 global.env = (name, def = null) => {
 	const val = process.env[name]
-	if (val == null) {
+	if (val == null || val === '') {
 		return def
 	}
 	return val
 }
 
+// Services should be defined as getters instead of directly requiring in the services
+// object to 1) prevent rewriting 2) lazy load. This bootstrap is also used for jeeves
+// and config files, so avoiding eager-loading of dependencies that are't used is a
+// performance consideration
 global.service = {
 	get logger() {
 		return require('./logger')
