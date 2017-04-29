7fcf165978604252e338e278c7c07d67bb0d6567
Louis Capitanchik <contact@louiscap.co>
2017-04-29 23:29:18 +0100

    Read/Write env files to the max, turbo style

--- console/lib/env-read-write.js
+++ console/lib/env-read-write.js
@@ -1,6 +1,8 @@
 const TYPE_COMMENT = 'comment'
 const TYPE_ENTRY = 'entry'
-const TYPE_ROOT = 'root'
+
+const CRLF = /\r\n/
+const LFCR = /\n\r/
 
 class Location {
 	constructor(line, column = 0) {
@@ -22,15 +24,24 @@ function comment(line, contents) {
 }
 function entry(line, contents) {
 	const [key, value] = contents.split('=').map(s => s.trim())
-	return new Node(new Location(line), TYPE_ENTRY, { key, value })
+	return new Node(new Location(line), TYPE_ENTRY, {key, value})
 }
+const serialiseEntry = ({ contents }) => `${contents.key}=${contents.value == null ? '' : contents.value}`
 
 class EnvironmentFile {
-	constructor(file) {
-		const lines = file.split('\n')
+	constructor(file, lineEndings = 'preserve') {
+		this.lineEnding = lineEndings !== 'preserve' ? lineEndings : (() => {
+			if (CRLF.test(file)) {
+				return '\r\n'
+			} else if (LFCR.test(file)) {
+				return '\n\r'
+			} else {
+				return '\n'
+			}
+		})()
 
 		const valueMap = {}
-		const nodes = lines.map((line, i) => {
+		const nodes = file.split(/\r\n|\n\r|\n/).map((line, i) => {
 			const trimmed = line.trim()
 			if (trimmed === '' || trimmed.startsWith('#')) {
 				return comment(i, line)
@@ -42,13 +53,65 @@ class EnvironmentFile {
 		})
 
 		Object.defineProperties(this, {
+			/** map is a relation of env file data key to node list index, for faster lookups of KV entries**/
 			map: { value: valueMap, writable: false, enumerable: true },
+			/** nodes is the parsed list of entries in the env file **/
 			nodes: { value: nodes, writable: false, enumerable: true },
 		})
 	}
 
 	toString() {
+		return this.nodes
+			.map(node => {
+				if (node.type === TYPE_COMMENT) {
+					return node.contents
+				} else if (node.type === TYPE_ENTRY) {
+					return serialiseEntry(node)
+				}
+				return false
+			})
+			.filter(node => typeof node === 'string')
+			.join(this.lineEnding)
+	}
 
+	value(key, value) {
+		if (typeof value !== 'undefined') {
+			if (key in this.map) {
+				this.nodes[this.map[key]].contents.value = value
+			} else {
+				const idx = this.nodes.length
+				this.nodes.push(entry(idx, { key, value }))
+				this.map[key] = idx
+			}
+			return undefined
+		} else {
+			if (key in this.map) {
+				return this.nodes[this.map[key]].contents.value
+			} else {
+				return undefined
+			}
+		}
+	}
+
+	entry(key) {
+		if (key in this.map) {
+			return serialiseEntry(this.nodes[this.map[key]])
+		}
+	}
+
+	remove(key) {
+		if (key in this.map) {
+			const idx = this.map[key]
+			delete this.map[key]
+			this.nodes = this.nodes.filter((_, i) => i !== idx)
+			this.map = Object.entries(this.map).reduce((acc, [key, pos]) => Object.assign(acc, {
+				[key]: pos > idx ? pos - 1 : pos
+			}), {})
+
+			return true
+		} else {
+			return false
+		}
 	}
 }
 
