95b6c9622c91ee9300a7f4b64956dfd8bbd37da7
Louis Capitanchik <contact@louiscap.co>
2017-05-15 20:30:17 +0100

    Partials

--- services/templates.js
+++ services/templates.js
@@ -5,14 +5,11 @@ const theme = require('./theme')
 const Defer = require('defer-class')
 
 module.exports = class TemplateRenderer {
-	static get hbs() {
-		return hbs
-	}
-
 	constructor(path, config) {
 		this.path = path
 		this.config = config
 		this.loading = new Defer()
+		this.hasInit = false
 
 		const renderer = hbs.create()
 		layout.register(renderer)
@@ -20,9 +17,11 @@ module.exports = class TemplateRenderer {
 	}
 
 	async init() {
+		this.hasInit = true
 		const path = await theme.getPath()
 		const fs = jetpack.cwd(path).dir('views')
 		const layouts = await fs.listAsync('layouts')
+		const partials = await fs.listAsync('partials')
 
 		const loadPartial = async (prefix, name, path) => {
 			const content = await fs.readAsync(path, 'utf8')
@@ -30,19 +29,23 @@ module.exports = class TemplateRenderer {
 		}
 
 		await Promise.all(layouts.map(layout => loadPartial('layouts', layout.split('.')[0], fs.path('layouts', layout))))
+		await Promise.all(partials.map(partial => loadPartial('partials', partial.split('.')[0], fs.path('partials', partial))))
 
 		this.loading.resolve()
 	}
 
 	async render(view, data) {
 		// Lock rendering anything until we've initialised
-		await this.loading.promise
+		if (!this.hasInit) {
+			await this.init()
+		} else {
+			await this.loading.promise
+		}
 
-		service.logger.info(this.renderer)
+		const viewData = Object.assign({ blog_name: env('SITE_NAME') }, data)
+
 		const template = await jetpack.readAsync(path.join(this.path, 'views', `${view}.hbs`), 'utf8')
 		const cmp = this.renderer.compile(template)
-		const t = cmp(data)
-		console.log(t)
-		return t
+		return cmp(viewData)
 	}
 }
